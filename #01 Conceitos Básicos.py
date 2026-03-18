62b9e1d1490a13f3d297e48a66d3245e6e2cc10d
Andreia Dias <andreiadias2311@gmail.com>
2026-03-18 19:11:04 -0300

    #01 Conceitos Básicos.py

---
+++ #01 Conceitos Básicos.py
@@ -0,0 +1,9 @@
+#pip install pandas
+#pip install jupyterlab
+
+#tipos de dados
+#dados estruturados: tabelas, planilhas, bancos de dados
+#dados não estruturados: textos, imagens, vídeos, áudios
+
+#Data Frame: estrutura de dados bidimensional, semelhante a uma tabela, onde os dados são organizados em linhas e colunas. 
+# Cada coluna pode conter um tipo de dado diferente (números, texto, datas, etc.), e cada linha representa uma entrada ou registro.
