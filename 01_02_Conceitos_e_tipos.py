971cd55a4b7a87225e60f23df30f362e47d80912
Andreia Dias <andreiadias2311@gmail.com>
2026-03-19 17:57:21 -0300

    01_02_Conceitos_e_tipos.py

--- 01_02_Conceitos_e_tipos.py
+++ 01_02_Conceitos_e_tipos.py
@@ -7,3 +7,8 @@
 
 #Data Frame: estrutura de dados bidimensional, semelhante a uma tabela, onde os dados são organizados em linhas e colunas. 
 # Cada coluna pode conter um tipo de dado diferente (números, texto, datas, etc.), e cada linha representa uma entrada ou registro.
+#==================================================
+##02 Tipos de Variáveis
+# Tipos de dados: 
+#numerico ( discreto: numero inteiros) e contínuo ( altura, peso, area, peso)
+#categórico (nominal: cor de cabelo, idioma, aprovado), ordinal (classificação: baixo, médio, alto) e identificador (cpf, rg, numero de serie)
